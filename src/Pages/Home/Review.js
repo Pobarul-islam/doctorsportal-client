a3bbf9db5eec66469cc98a67c6c20bf29966d2c6
Pobarul-islam <pobarul3@gmail.com>
2022-07-16 10:01:11 +0600

    testimonial added

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
-const Review = ({review}) => {
+const Review = ({ review }) => {
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-  <div class="card-body">
-    
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae atque amet rem maiores, vitae perferendis repudiandae quas adipisci quidem porro.</p>
-    <div class="flex items-center">
-    <div class="avatar">
-  <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-    <img src={review.img} />
-    </div>
-    <div>
-    <h4 className='text-xl'>{ review.name}</h4>
-    <p>{ review.location}</p>
-</div>
-</div>
-                    
-                   
-                
-    </div>
-  </div>
-</div>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, voluptates quo incidunt harum minus mollitia!</p>
+                <div className="flex items-center">
+                    <div className="avatar">
+                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                            <img src={review.img} alt=""/>
+                        </div>
+                    </div>
+                    <div>
+                        <h4 className='text-xl'>{review.name}</h4>
+                        <p>{review.location}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
