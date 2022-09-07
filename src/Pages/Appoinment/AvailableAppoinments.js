c366c8ce15f8ba73420648169f60fc20d9c27163
Pobarul-islam <pobarul3@gmail.com>
2022-09-07 13:44:58 +0600

    slots added

--- src/Pages/Appoinment/AvailableAppoinments.js
+++ src/Pages/Appoinment/AvailableAppoinments.js
@@ -1,11 +1,29 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Service from './Service';
 
-const AvailableAppoinments = ({date}) => {
+const AvailableAppoinments = ({ date }) => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch("services.json")
+            .then(res => res.json())
+            .then(data=>setServices(data))
+    },[])
     
     return (
         <div>
-            <h4 className='text-xl text-secondary font-bold text-center'>Availabe appoinments on : {format (date, "PP")}</h4>
+            <h4 className='text-xl text-secondary font-bold text-center'>Availabe appoinments on : {format(date, "PP")}</h4>
+            
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                  {
+                services.map(service => <Service
+                    key={service._id}
+                    service={service}
+                ></Service>)
+            }
+          </div>
         </div>
     );
 };
---
+++ src/Pages/Appoinment/Service.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+    const { name, slots } = service;
+    return (
+       <div class="card  w-96 ml-6 mt-5 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title text-secondary">{name}</h2>
+                <p>{slots.length > 0
+                    ? <span>{slots[0]} </span>
+                    : <span className='text-red-500'>Try another date.</span>
+                }
+                </p>
+                
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available </p>
+    <div class="card-actions justify-center">
+      <button disabled={slots.length=== 0 } class="btn btn-secondary text-white uppercase">Book Appoinment</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Service;
