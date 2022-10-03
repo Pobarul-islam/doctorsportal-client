ecfc3984bbe1186f53993108769ef0661f98d414
Pobarul-islam <pobarul3@gmail.com>
2022-10-03 12:24:28 +0600

    services code updated

--- src/Pages/Appoinment/AvailableAppoinments.js
+++ src/Pages/Appoinment/AvailableAppoinments.js
@@ -1,25 +1,24 @@
 import { format } from "date-fns";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BookingModal from "./BookingModal";
 import Service from "./Service";
 
-const AvailableAppoinments = ({ date }) => {
+const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
+
+  const formattedDate = format(date, "PP");
   useEffect(() => {
-    fetch("http://localhost:5000/service")
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [formattedDate]);
 
   return (
-    <div>
-      <h4 className="text-xl text-secondary font-bold text-center">
-        Availabe appoinments on : {format(date, "PP")}
+    <div className="my-10">
+      <h4 className="text-xl text-secondary text-center my-12">
+        Available Appointments on {format(date, "PP")}
       </h4>
-
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {services.map((service) => (
           <Service
@@ -40,4 +39,4 @@ const AvailableAppoinments = ({ date }) => {
   );
 };
 
-export default AvailableAppoinments;
+export default AvailableAppointments;
--- src/Pages/Appoinment/Service.js
+++ src/Pages/Appoinment/Service.js
@@ -3,29 +3,27 @@ import React from "react";
 const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
-    <div className="card  w-96 ml-6 mt-5 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title text-secondary">{name}</h2>
+    <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+      <div className="card-body text-center">
+        <h2 className="text-xl font-bold text-secondary">{name}</h2>
         <p>
           {slots.length > 0 ? (
-            <span>{slots[0]} </span>
+            <span>{slots[0]}</span>
           ) : (
             <span className="text-red-500">Try another date.</span>
           )}
         </p>
-
         <p>
-          {slots.length} {slots.length > 1 ? "spaces" : "space"} available{" "}
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
         <div className="card-actions justify-center">
-          <button className=""></button>
           <label
             htmlFor="booking-modal"
             disabled={slots.length === 0}
             onClick={() => setTreatment(service)}
-            className="btn btn-secondary text-white uppercase"
+            className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
           >
-            Book Appoinment
+            Book Appointment
           </label>
         </div>
       </div>
