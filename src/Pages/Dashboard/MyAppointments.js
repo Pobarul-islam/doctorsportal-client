5c512c5ed5ba7c43b85068a6c5b528887ff58bb4
Pobarul-islam <pobarul3@gmail.com>
2022-10-04 19:42:40 +0600

    my appointments data loaded

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+    const [Appointments, setAppointments] = useState([]);
+      const [user] = useAuthState(auth);
+
+     useEffect(() => {
+       if (user) {
+         fetch(`http://localhost:5000/booking?patient=${user.email}`)
+           .then((res) => res.json())
+           .then((data) => setAppointments(data));
+       }
+     }, [user]);
     return (
-        <div>
-            <h2>My Appointments</h2>
-        </div>
+      <div>
+        <h2>My Appointments: {Appointments.length}</h2>
+       
+      </div>
     );
 };
 
